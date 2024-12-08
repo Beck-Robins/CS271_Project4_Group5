cd344170bfd43b068fb9e13c2e4956c330d2ec4d
Beck-Robins <143750988+Beck-Robins@users.noreply.github.com>
2024-12-07 22:14:54 -0500

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -4,13 +4,17 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include <tuple>
+#include <iostream>
 #include <stdexcept>
+#include <sstream>
+#include <queue>
 #include "customexceptions.hpp"
 
 class Graph {
 private:
     std::unordered_map<int, std::unordered_set<int>> adjacencyList;
-    std::vector<int> sorted;
+    std::vector<int> sorted;  // Stores topologically sorted vertices
 
 public:
     // Constructors and Destructors
@@ -19,15 +23,24 @@ public:
     Graph& operator=(const Graph& other);
     ~Graph() = default;
 
+    // Methods
     void addEdge(int u, int v);
     void removeEdge(int u, int v);
     bool edgeIn(int u, int v);
     void deleteVertex(int u);
     void addVertex(int u);
 
+    // Search Algorithms
+    std::unordered_map<int, std::pair<int, int>> breadthFirstSearch(int s);
+    std::unordered_map<int, std::tuple<int, int, int>> depthFirstSearch(bool sort = false);
+
+    // Utility Methods
     void sortVertices();
     const std::vector<int>& getSortedVertices() const;
+    std::vector<int> getOrdering() const;
+
+    // Input/Output
+    static Graph readFromSTDIN();
 };
 
-#endif // GRAPH_HPP
-
+#endif
