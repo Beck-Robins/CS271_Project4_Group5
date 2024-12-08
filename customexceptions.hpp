fa5de3fa96813d9435b730be4f6d082cb7313660
Beck-Robins <143750988+Beck-Robins@users.noreply.github.com>
2024-12-07 21:39:37 -0500

    Update customexceptions.hpp

--- customexceptions.hpp
+++ customexceptions.hpp
@@ -1,1 +1,20 @@
+#ifndef CUSTOMEXCEPTIONS_HPP
+#define CUSTOMEXCEPTIONS_HPP
+
+#include <stdexcept>
+#include <string>
+
+class EdgeException : public std::runtime_error {
+public:
+    explicit EdgeException(const std::string& message)
+        : std::runtime_error("EdgeException: " + message) {}
+};
+
+class VertexException : public std::runtime_error {
+public:
+    explicit VertexException(const std::string& message)
+        : std::runtime_error("VertexException: " + message) {}
+};
+
+#endif
 
