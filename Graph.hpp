6d74185d9122dbe737b09ea45c1f6238d648b7bc
Beck-Robins <143750988+Beck-Robins@users.noreply.github.com>
2024-12-07 21:41:22 -0500

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -1,1 +1,33 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <stdexcept>
+#include "customexceptions.hpp"
+
+class Graph {
+private:
+    std::unordered_map<int, std::unordered_set<int>> adjacencyList;
+    std::vector<int> sorted;
+
+public:
+    // Constructors and Destructors
+    Graph() = default;
+    Graph(const Graph& other);
+    Graph& operator=(const Graph& other);
+    ~Graph() = default;
+
+    void addEdge(int u, int v);
+    void removeEdge(int u, int v);
+    bool edgeIn(int u, int v);
+    void deleteVertex(int u);
+    void addVertex(int u);
+
+    void sortVertices();
+    const std::vector<int>& getSortedVertices() const;
+};
+
+#endif // GRAPH_HPP
 
